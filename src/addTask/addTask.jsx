a5930cc1ecff55b047c137718f85d2826c05bd51
jocelynperucca <jocelynperucca@gmail.com>
2024-11-02 19:19:21 -0600

    converted addTask to react

--- src/addTask/addTask.jsx
+++ src/addTask/addTask.jsx
@@ -1,9 +1,36 @@
 import React from 'react';
+import './addTask.css'
 
 export function AddTask() {
   return (
-    <main className='container-fluid bg-secondary text-center'>
-      <div>addTask displayed here</div>
-    </main>
-  );
+        <main>
+            <div>Task User:</div> 
+            <p>Jocelyn Titus</p>
+
+            <br />
+
+            {/* Add task with priority */}
+            <div>
+                <label htmlFor="addtask">Add Task</label>
+                <input type="text" placeholder="enter task" id="addtask" />
+            </div>
+
+            {/* Priority selection */}
+            <h3>Priority</h3>
+            <input type="radio" id="priority1" name="priority" value="High" />
+            <label htmlFor="priority1"> High</label><br />
+            <input type="radio" id="priority2" name="priority" value="Medium" />
+            <label htmlFor="priority2"> Medium</label><br />
+            <input type="radio" id="priority3" name="priority" value="Low" />
+            <label htmlFor="priority3"> Low</label><br />
+
+            <br />
+
+            <div>
+                <button>Add</button>
+            </div>
+
+            <br />
+        </main>
+    );
 }
