4ebdbf7575f22b932afeb59bdfe6f395857f690f
jocelynperucca <jocelynperucca@gmail.com>
2024-11-12 17:37:38 -0700

    index.js working

--- service/index.js
+++ service/index.js
@@ -1,12 +1,53 @@
 const express = require('express');
 const app = express();
+const uuid = require('uuid');
+
+let users = {};
+let scores = [];
 
 const port = process.argv.length > 2 ? process.argv[2] : 3000;
 
-app.get('*', (_req, res) => {
-  res.send({ msg: 'Startup Service' });
-});
+app.use(express.json());
+
+var apiRouter = express.Router();
+app.use(`/api`, apiRouter);
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
+
+// CreateAuth a new user
+apiRouter.post('/auth/create', async (req, res) => {
+    const user = users[req.body.email];
+    if (user) {
+      res.status(409).send({ msg: 'Existing user' });
+    } else {
+      const user = { email: req.body.email, password: req.body.password, token: uuid.v4() };
+      users[user.email] = user;
+  
+      res.send({ token: user.token });
+    }
+  });
+  
+  // GetAuth login an existing user
+  apiRouter.post('/auth/login', async (req, res) => {
+    const user = users[req.body.email];
+    if (user) {
+      if (req.body.password === user.password) {
+        user.token = uuid.v4();
+        res.send({ token: user.token });
+        return;
+      }
+    }
+    res.status(401).send({ msg: 'Unauthorized' });
+  });
+  
+  // DeleteAuth logout a user
+  apiRouter.delete('/auth/logout', (req, res) => {
+    const user = Object.values(users).find((u) => u.token === req.body.token);
+    if (user) {
+      delete user.token;
+    }
+    res.status(204).end();
+  });
+  
